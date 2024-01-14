5ba30701f38170a307eb509b5b9de909dbbfc513
Rafael <rafaelmagalhaes1@live.com>
2024-01-14 12:08:53 -0300

    initial commit

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,6 +1,8 @@
 const express = require('express');
+const routes = require('./routes');
 
 const app = express();
+app.use(routes);
 
 // não remova esse endpoint, é para o avaliador funcionar
 app.get('/', (_request, response) => {
