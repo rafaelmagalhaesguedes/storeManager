b22b7f85817033c8a529ea231a6ce4b76f0de9ef
Rafael <rafaelmagalhaes1@live.com>
2024-01-15 09:46:09 -0300

    feat: update barrel on routes

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -1,9 +1,11 @@
 // Barrel Routes
 const express = require('express');
 const productsRoutes = require('./products.routes');
+const salesRoutes = require('./sales.routes');
 
 const router = express.Router();
 
 router.use(productsRoutes);
+router.use(salesRoutes);
 
 module.exports = router;
