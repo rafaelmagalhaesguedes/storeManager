68365f9849201b4b46a338141cefe12f16f0c2e6
Rafael <rafaelmagalhaes1@live.com>
2024-01-15 19:41:35 -0300

    fix: add router.use(express.json()) on routes files

--- backend/src/routes/products.routes.js
+++ backend/src/routes/products.routes.js
@@ -2,11 +2,15 @@ const express = require('express');
 const productsController = require('../controllers/products.controller');
 
 const router = express.Router();
+router.use(express.json());
 
 // Rota de produtos
 router.get('/products', productsController.getAllProducts);
 
 // Rota de produtos por id
 router.get('/products/:id', productsController.getProductById);
+
+// Rota de criação de produtos
+router.post('/products', productsController.createProduct);
 
 module.exports = router;
--- backend/src/routes/sales.routes.js
+++ backend/src/routes/sales.routes.js
@@ -2,6 +2,7 @@ const express = require('express');
 const salesController = require('../controllers/sales.controller');
 
 const router = express.Router();
+router.use(express.json());
 
 router.get('/sales', salesController.getAllSales);
 
