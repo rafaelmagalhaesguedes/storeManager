1218518098bfb335fac648e3761375d3fdcec8b9
Rafael <rafaelmagalhaes1@live.com>
2024-01-15 19:39:34 -0300

    feat: create function to createProduct on controller layer

--- backend/src/controllers/products.controller.js
+++ backend/src/controllers/products.controller.js
@@ -19,7 +19,24 @@ const getProductById = async (req, res) => {
   }
 };
 
+const createProduct = async (req, res) => {
+  const { name } = req.body;
+  try {
+    if (!name) {
+      return res.status(400).json({ message: '"name" is required' });
+    }
+    if (name.length < 5) {
+      return res.status(422).json({ message: '"name" length must be at least 5 characters long' });
+    }
+    const product = await productsService.createProduct(name);
+    res.status(201).json(product);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
 module.exports = {
   getAllProducts,
   getProductById,
+  createProduct,
 };
