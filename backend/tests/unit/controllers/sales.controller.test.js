ec35e0f524ce7057df423831f09cbb838cf64f55
Rafael <rafaelmagalhaes1@live.com>
2024-01-16 10:12:35 -0300

    feat: create tests to createSales function on controller layer

--- backend/tests/unit/controllers/sales.controller.test.js
+++ backend/tests/unit/controllers/sales.controller.test.js
@@ -63,4 +63,69 @@ describe('Sales Controller', function () {
       expect(res.status).to.have.been.calledWith(404);
     });
   });
+
+  describe('createSale', function () {
+    it('should create a sale and return status 201', async function () {
+      const req = {
+        body: [
+          {
+            productId: 1,
+            quantity: 1,
+          },
+          {
+            productId: 2,
+            quantity: 5,
+          },
+        ],
+      };
+
+      const res = {
+        status: sinon.stub().returnsThis(),
+        json: sinon.stub(),
+      };
+
+      const sale = {
+        id: 3,
+        itemsSold: req.body,
+      };
+
+      sinon.stub(salesService, 'createSale').resolves(sale);
+
+      await salesController.createSale(req, res);
+
+      expect(res.status.calledOnceWith(201));
+      expect(res.json.calledOnceWith(sale));
+
+      salesService.createSale.restore();
+    });
+
+    it('should return status 500 on error', async function () {
+      const req = {
+        body: [
+          {
+            productId: 1,
+            quantity: 1,
+          },
+          {
+            productId: 2,
+            quantity: 5,
+          },
+        ],
+      };
+
+      const res = {
+        status: sinon.stub().returnsThis(),
+        json: sinon.stub(),
+      };
+
+      sinon.stub(salesService, 'createSale').rejects(new Error('Error'));
+
+      await salesController.createSale(req, res);
+
+      expect(res.status.calledOnceWith(500));
+      expect(res.json.calledOnceWith({ message: 'Error' }));
+
+      salesService.createSale.restore();
+    });
+  });
 });
