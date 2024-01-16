70be431bb14200131f012ee79c1b7cd361ac1da5
Rafael <rafaelmagalhaes1@live.com>
2024-01-16 08:34:51 -0300

    feat: create products.validation.test

---
+++ backend/tests/unit/middlewares/products.validation.test.js
@@ -0,0 +1,19 @@
+const sinon = require('sinon');
+const { expect } = require('chai');
+const productsValidation = require('../../../src/middlewares/products.validation');
+
+describe('Products Validation Middleware', function () {
+  it('should call next when name is valid', function () {
+    const req = {
+      body: {
+        name: 'T-Shirt Cruzeiro Esporte Clube',
+      },
+    };
+    const res = {};
+    const next = sinon.stub();
+
+    productsValidation.validateProductName(req, res, next);
+
+    expect(next.calledOnce).to.equal(true);
+  });
+});
