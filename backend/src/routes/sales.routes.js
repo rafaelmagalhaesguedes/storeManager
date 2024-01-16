00cd5c06d2f75d7ec90d2af0ee30bb7fa7430101
Rafael <rafaelmagalhaes1@live.com>
2024-01-16 10:12:05 -0300

    feat: create end point POST /sales to create a new Sale

--- backend/src/routes/sales.routes.js
+++ backend/src/routes/sales.routes.js
@@ -4,8 +4,13 @@ const salesController = require('../controllers/sales.controller');
 const router = express.Router();
 router.use(express.json());
 
+// Route to get all sales
 router.get('/sales', salesController.getAllSales);
 
+// Route to get a sale by id
 router.get('/sales/:id', salesController.getSaleById);
+
+// Route to create a sale
+router.post('/sales', salesController.createSale);
 
 module.exports = router;
