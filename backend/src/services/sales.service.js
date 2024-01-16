a9b12da2838e71d8b4ff7d4bc52feae07bc18239
Rafael <rafaelmagalhaes1@live.com>
2024-01-16 10:10:44 -0300

    feat: add createSale function on service layer

--- backend/src/services/sales.service.js
+++ backend/src/services/sales.service.js
@@ -2,17 +2,29 @@ const salesModel = require('../models/sales.model');
 
 const findAllSales = async () => {
   const sales = await salesModel.findAllSales();
-  
   return sales;
 };
 
 const findSaleById = async (id) => {
   const sale = await salesModel.findSaleById(id);
-  
   return sale;
+};
+
+const createSale = async (itemsSold) => {
+  const sale = await salesModel.createSale();
+  
+  const itemsSoldPromises = itemsSold.map((item) => {
+    const { productId, quantity } = item;
+    return salesModel.createSaleProduct(sale.insertId, productId, quantity);
+  });
+
+  await Promise.all(itemsSoldPromises);
+
+  return { id: sale.insertId, itemsSold };
 };
 
 module.exports = {
   findAllSales,
   findSaleById,
+  createSale,
 };
