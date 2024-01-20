c310ebd0982b527d21c9060a2fbf6439835a4cbf
Rafael <rafaelmagalhaes1@live.com>
2024-01-20 18:42:32 -0300

    feat: create updateSalesProductQuantity on service layer

--- backend/src/services/sales.service.js
+++ backend/src/services/sales.service.js
@@ -24,9 +24,27 @@ const createSale = async (itemsSold) => {
 };
 
 const deleteSale = async (id) => {
+  const [sale] = await salesModel.findSale(id);
+  
+  if (!sale) throw new Error('Sale not found');
+
   const result = await salesModel.deleteSale(id);
 
   return result;
+};
+
+const updateSaleProductQuantity = async (saleId, productId, quantity) => {
+  const [sale] = await salesModel.findSale(saleId);
+
+  if (!sale) throw new Error('Sale not found');
+
+  const [product] = await salesModel.findProduct(productId);
+  
+  if (!product) throw new Error('Product not found in sale');
+
+  const result = await salesModel.updateSaleProductQuantity(saleId, productId, quantity);
+
+  return result[0];
 };
 
 module.exports = {
@@ -34,4 +52,5 @@ module.exports = {
   findSaleById,
   createSale,
   deleteSale,
+  updateSaleProductQuantity,
 };
