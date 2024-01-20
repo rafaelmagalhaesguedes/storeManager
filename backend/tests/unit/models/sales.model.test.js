765587cb6495062a802efd18dd040940e32feeea
Rafael <rafaelmagalhaes1@live.com>
2024-01-20 07:55:32 -0300

    fix: tests sales.model fixed

--- backend/tests/unit/models/sales.model.test.js
+++ backend/tests/unit/models/sales.model.test.js
@@ -1,38 +1,49 @@
 const chai = require('chai');
 const sinon = require('sinon');
 const salesModel = require('../../../src/models/sales.model');
+const connection = require('../../../src/models/connection');
 
 const { expect } = chai;
 
 describe('Sales Model', function () {
-  describe('findAllSales', function () {
-    it('should return all sales', async function () {
-      const mockSales = [
-        { saleId: 1, date: '2021-09-09T04:54:29.000Z', productId: 1, quantity: 2 },
-      ];
-      
-      const stub = sinon.stub(salesModel, 'findAllSales').returns(mockSales);
-      
-      const result = await salesModel.findAllSales();
-      
-      expect(result).to.eql(mockSales);
-      
+  describe('findAllSales()', function () {
+    it('should return an array of sales', async function () {
+      const mockSales = [{ id: 1, productId: 1, quantity: 1 }, { id: 2, productId: 2, quantity: 2 }];
+
+      const stub = sinon.stub(connection, 'execute').resolves([mockSales]);
+
+      const sales = await salesModel.findAllSales();
+
+      expect(sales).to.eql(mockSales);
+
       stub.restore();
     });
   });
 
-  describe('findSaleById', function () {
-    it('should return a sale by id', async function () {
-      const mockSale = [
-        { date: '2021-09-09T04:54:29.000Z', productId: 1, quantity: 2 },
-      ];
-      
-      const stub = sinon.stub(salesModel, 'findSaleById').returns(mockSale);
-      
-      const result = await salesModel.findSaleById(1);
-      
-      expect(result).to.eql(mockSale);
-      
+  describe('findSaleById()', function () {
+    it('should return a sale', async function () {
+      const mockSale = { id: 1, productId: 1, quantity: 1 };
+
+      const stub = sinon.stub(connection, 'execute').resolves([[mockSale]]);
+
+      const sale = await salesModel.findSaleById(1);
+
+      expect(sale).to.eql(mockSale);
+
+      stub.restore();
+    });
+  });
+
+  describe('createSale()', function () {
+    it('should create a sale', async function () {
+      const mockSale = { id: 1 };
+
+      const stub = sinon.stub(connection, 'execute').resolves([{ id: 1 }]);
+
+      const sale = await salesModel.createSale();
+
+      expect(sale).to.eql(mockSale);
+
       stub.restore();
     });
   });
