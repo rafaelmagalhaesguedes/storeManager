df1c93e9a1495180edab4e1277c1f44e3c53a881
Rafael <rafaelmagalhaes1@live.com>
2024-01-21 13:27:37 -0300

    feat: create searchProduct on service layer

--- backend/src/services/products.service.js
+++ backend/src/services/products.service.js
@@ -1,47 +1,71 @@
 const productsModel = require('../models/products.model');
 
 const findAllProducts = async () => {
+  // Return all products
   const products = await productsModel.findAllProducts();
   
   return products;
 };
 
 const findProductById = async (id) => {
+  // Check if product exists
   const product = await productsModel.findProductById(id);
 
+  // If product does not exist, throw an error
   if (!product) throw new Error('Product not found');
   
   return product;
 };
 
 const createProduct = async (name) => {
+  // Check if product name is empty
   const product = await productsModel.createProduct(name);
 
+  // If product name is empty, throw an error
   if (!product) throw new Error('Product name is required');
   
   return product;
 };
 
 const updateProduct = async (id, name) => {
+  // Check if product exists
   const product = await productsModel.findProductById(id);
 
+  // If product does not exist, throw an error
   if (!product) throw new Error('Product not found');
   
+  // If product exists, update it
   await productsModel.updateProduct(id, name);
 
+  // Return the updated product
   const updatedProduct = await productsModel.findProductById(id);
-
   return updatedProduct;
 };
 
 const deleteProduct = async (id) => {
+  // Check if product exists
   const product = await productsModel.findProductById(id);
 
+  // If product does not exist, throw an error
   if (!product) throw new Error('Product not found');
   
+  // If product exists, delete it
   const result = await productsModel.deleteProduct(id);
 
+  // Return the result
   return result;
+};
+
+const searchProduct = async (name) => {
+  // Name is empty, return all products
+  if (!name) {
+    const products = await productsModel.findAllProducts();
+    return products;
+  }
+
+  // Name is not empty, search products by name
+  const products = await productsModel.searchProduct(name);
+  return products;
 };
 
 module.exports = {
@@ -50,4 +74,5 @@ module.exports = {
   createProduct,
   updateProduct,
   deleteProduct,
+  searchProduct,
 };
